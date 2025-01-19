cc192c60839889d9ec2bb077f5b66c7fa52fe842
Arroyo75 <jszczepanski75@gmail.com>
2025-01-19 23:03:21 +0100

    Product model

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+namespace price_comparator_site.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContexy(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .hasIndex(p => new { p.Name, p.StoreSource })
+                .isUnique();
+        }
+    }
+}
