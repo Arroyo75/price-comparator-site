fd46b55514a3e1635ee106553a3a546bcdc5109a
Arroyo75 <jszczepanski75@gmail.com>
2025-01-20 23:21:09 +0100

    IScraperService

---
+++ Services/Interfaces/IProductScraperService.cs
@@ -0,0 +1,11 @@
+﻿using price_comparator_site.Models;
+
+namespace price_comparator_site.Services.Interfaces
+{
+    public interface IProductScraperService
+    {
+        Task<Product> ScrapeProductAsync(string url);
+        Task<List<Product>> SearchProductsAsync(string searchTerm);
+        string StoreName { get; }
+    }
+}
